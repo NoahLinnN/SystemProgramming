b312c5d5198a839d8871bc290af554e204e1f1b1
alder <508303@student.saxion.nl>
2023-03-07 15:34:56 +0100

    failed attempt at doing stuff

--- week2/Assignment_2.2.ino
+++ week2/Assignment_2.2.ino
@@ -1,6 +1,10 @@
-#define LED_GREEN 2
-#define LED_YELLOW 3
-#define LED_RED 4
+#define LED_GREEN_PEDESTRIAN 2
+#define LED_RED_PEDESTRIAN 4
+#define BUTTON 5
+#define LED_GREEN_CAR 6
+#define LED_YELLOW_CAR 7
+#define LED_RED_CAR 8
+int buttonState = 0;
 
 void pause(int ms) {
   uint32_t start = millis();
@@ -11,24 +15,52 @@ void pause(int ms) {
 void setup() {
   Serial.begin(9600);
 
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN_PEDESTRIAN, OUTPUT);
+  pinMode(LED_RED_PEDESTRIAN, OUTPUT);
+  pinMode(LED_GREEN_CAR, OUTPUT);
+  pinMode(LED_RED_CAR, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
-  //Turn on light
-  // digitalWrite(LEDNAME, HIGH);
-  //Turn off light
-  // digitalWrite(LEDNAME, LOW);
+  int buttonState = digitalRead(BUTTON);
+  digitalWrite(LED_RED_PEDESTRIAN, HIGH);
+  digitalWrite(LED_GREEN_CAR, HIGH);
+  // if (buttonState == LOW) {
+  //   digitalWrite(LED_RED_PEDESTRIAN, HIGH);
+  //   Serial.println("button not pressed");
+  // } else {
+  //   Serial.println("button pressed");
+  // }
+  // //Turn on light
+  // // digitalWrite(LEDNAME, HIGH);
+  // //Turn off light
+  // // digitalWrite(LEDNAME, LOW);
 
-  digitalWrite(LED_GREEN, HIGH);
-  digitalWrite(LED_YELLOW, HIGH);
-  digitalWrite(LED_RED, HIGH);
-  pause(5000);   
-  digitalWrite(LED_GREEN, LOW);  
-  digitalWrite(LED_YELLOW, LOW); 
-  digitalWrite(LED_RED, LOW); 
-  pause(5000);  
-
+  // digitalWrite(LED_GREEN_PEDESTRIAN, HIGH);
+  // digitalWrite(LED_RED_PEDESTRIAN, HIGH);
+  // pause(5000);
+  // digitalWrite(LED_GREEN_PEDESTRIAN, LOW);
+  // digitalWrite(LED_RED_PEDESTRIAN, LOW);
+  // pause(5000);
+  // bool greenPedestrianIsOn = false;
+  while (true) {
+    int buttonState = digitalRead(BUTTON);
+    while (buttonState == LOW) {
+      digitalWrite(LED_GREEN_PEDESTRIAN, HIGH);
+      digitalWrite(LED_RED_PEDESTRIAN, LOW);
+      digitalWrite(LED_GREEN_CAR, LOW);
+      digitalWrite(LED_RED_CAR, HIGH);
+      Serial.println("button is pressed");
+      int buttonState = digitalRead(BUTTON);
+    }
+    while (buttonState == HIGH) {
+      digitalWrite(LED_RED_PEDESTRIAN, HIGH);
+      digitalWrite(LED_GREEN_PEDESTRIAN, LOW);
+      digitalWrite(LED_GREEN_CAR, HIGH);
+      digitalWrite(LED_RED_CAR, LOW);
+      Serial.println("button is not pressed");
+      int buttonState = digitalRead(BUTTON);
+    }
+  }
 }
